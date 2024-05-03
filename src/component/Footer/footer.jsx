827d2e83584bb071d9d3b889a84bef7b0ec50f64
ccandradewi <cornelia.candradewi@outlook.com>
2024-05-03 22:41:29 +0700

    fix name company

--- src/component/Footer/footer.jsx
+++ src/component/Footer/footer.jsx
@@ -67,7 +67,7 @@ function Footer() {
         <div className="flex flex-wrap items-center md:justify-between justify-center">
           <div className="w-full md:w-4/12 px-4 mx-auto text-center">
             <div className="text-sm text-blueGray-500 font-semibold py-1">
-              Copyright © 2021Notus JS by Creative Tim
+              Copyright © 2024 Mizuki Sushi Indonesia
             </div>
           </div>
         </div>
