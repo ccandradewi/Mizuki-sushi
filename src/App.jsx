76025da27620f602f586c5d750d522ca5fdc0f05
ccandradewi <cornelia.candradewi@outlook.com>
2024-05-03 22:36:48 +0700

    fix route

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,66 @@
 import "./App.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  Outlet,
+  RouterProvider,
+  ScrollRestoration,
+  useLocation,
+} from "react-router-dom";
 import AboutPage from "./pages/AboutPage";
 import HomePage from "./pages/HomePage";
 import ProductPage from "./pages/ProductPage";
 import TeamsPage from "./pages/TeamsPage";
+import { useEffect } from "react";
+const AppLayout = () => {
+  return (
+    <>
+      <ScrollRestoration />
+      <Outlet />
+    </>
+  );
+};
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <HomePage />,
-  },
-  {
-    path: "/about",
-    element: <AboutPage />,
-  },
-  {
-    path: "/products",
-    element: <ProductPage />,
-  },
-  {
-    path: "/teams",
-    element: <TeamsPage />,
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+      {
+        path: "/about",
+        element: <AboutPage />,
+      },
+      {
+        path: "/products",
+        element: <ProductPage />,
+      },
+      {
+        path: "/teams",
+        element: <TeamsPage />,
+      },
+    ],
   },
 ]);
 function App() {
   return <RouterProvider router={router} />;
 }
+function ScrollToTop() {
+  const { pathname } = useLocation();
 
-export default App;
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
+
+export default function AppWithScrollToTop() {
+  return (
+    <RouterProvider router={router}>
+      <ScrollToTop />
+      <App />
+    </RouterProvider>
+  );
+}
